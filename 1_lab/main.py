bdcc1608719db95e21043e2e324dc5c8e7c70086
UraN9 <kwuvezzzkuy@gmail.com>
2025-09-16 12:32:12 +0300

    Refactor word guessing game logic and enhance test coverage for letter checking functionality

--- 1_lab/main.py
+++ 1_lab/main.py
@@ -1,7 +1,8 @@
 WORDS = ["apple", "banana", "cherry", "orange", "Python"]
 
 from random import choice
-from typing import List
+from typing import List, Set
+
 def choose_secret_word(words: List[str]) -> str:
     return choice(words)
 
@@ -9,5 +10,28 @@ def enter_letter_from_user() -> str:
     letter = input("Введіть одну літеру: ").lower()
     return letter
 
-def check_letter_in_word(letter: str, word: str) -> str:
-    return letter in word
+def check_letters_in_word(letters: Set[str], word: str) -> str:
+    if word == "":
+        raise ValueError("Слово не має бути порожнім")
+    return "".join([l if l in letters else "*" for l in word])
+
+def check_if_word_guessed(letters: Set[str], word: str) -> bool:
+    return all(l in letters for l in word)
+
+def main():
+    secret_word = choose_secret_word(WORDS)
+    # print(f"Загадане слово: {secret_word}")
+    entered_user_letters = set()
+    for _ in range(len(secret_word) + 3):
+        entered_user_letters.add(enter_letter_from_user())
+        result = check_letters_in_word(entered_user_letters, secret_word)
+        print(f"Результат перевірки літери '{entered_user_letters}' у слові: {result}")
+        if check_if_word_guessed(entered_user_letters, secret_word):
+            print(f"Ви вгадали слово !")
+            break
+    if not check_if_word_guessed(entered_user_letters, secret_word):
+        print(f"Ви не вгадали слово !")
+    print("Гру завершено! загадане слово було:", secret_word)
+
+if __name__ == "__main__":
+    main()
--- 1_lab/test.py
+++ 1_lab/test.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch
-from main import choose_secret_word, enter_letter_from_user, WORDS
+from main import *
 
 class TestWordChoice(unittest.TestCase):
     def test_word_in_list(self):
@@ -39,6 +39,46 @@ class TestEnterLetterFromUser(unittest.TestCase):
         #     self.assertEqual(enter_letter_from_user(), 'a')
         # finally:
         #     __builtins__.input = original_input
+
+class TestCheckLettersInWord(unittest.TestCase):
+    def test_all_letters_guessed(self):
+        """
+        Даний тест є валідний"""
+        test_word = 'apple'
+        self.assertEqual(
+            check_letters_in_word (set(test_word), test_word),
+            test_word
+        )
+
+    def test_no_letters_guessed(self):
+        self.assertEqual(check_letters_in_word(set(), 'banana'), '******')
+
+    def test_some_letters_guessed(self):
+        self.assertEqual(check_letters_in_word({'a', 'n'}, 'banana'), '*anana')
+
+    def test_repeated_letters(self):
+        self.assertEqual(check_letters_in_word({'b', 'a'}, 'banana'), 'ba*a*a')
+
+    def test_empty_letters(self):
+        """
+        Даний тест ми також залишимо та трішки модифікуємо"""
+        test_word = 'test'
+        guess_letters = set("")
+        with self.assertRaises(ValueError):
+            check_letters_in_word(guess_letters, "")
+
+    def test_empty_word(self):
+        guess_letters = set("")
+        # Виловлюємо Помилку
+        with self.assertRaises(ValueError):
+            check_letters_in_word(guess_letters, "")
+        # Перевіряємо текст помилки, що це саме наша помилка яку ми написали
+        with self.assertRaises(ValueError) as context:
+            check_letters_in_word(guess_letters, "")
+            self.assertEqual(str(context.exception), "Слово не має бути порожнім")
+
+    def test_letters_not_in_word(self):
+        self.assertEqual(check_letters_in_word({'x', 'y', 'z'}, 'orange'), '******')
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
