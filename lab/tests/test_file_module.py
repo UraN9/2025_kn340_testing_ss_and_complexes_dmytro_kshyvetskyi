020ae9b212403c8da51347e3a3e89ddab2898567
UraN9 <kwuvezzzkuy@gmail.com>
2025-11-11 09:45:21 +0200

    Почали працювати з coverage

--- lab/tests/test_file_module.py
+++ lab/tests/test_file_module.py
@@ -1,3 +1,4 @@
+import pytest
 from lab.file_module import get_n_random_words
 
 def test_get_n_random_words():
@@ -6,3 +7,12 @@ def test_get_n_random_words():
     for n in range(1, 6):
         words = get_n_random_words(n)
         assert len(words) == n, f"Очікувалось {n} слів, отримано {len(words)}"
+
+def test_get_n_random_words_raises_value_error():
+    """
+    Перевіряємо чи функція піднімає ValueError коли ми перевищуємо доступну кількість слів
+    """
+    invalid_inputs = [50]
+    with pytest.raises(ValueError):
+        for n in invalid_inputs:
+            get_n_random_words(n)
