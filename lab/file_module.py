25d9426e98c732a8ef6c4be898b1e2923f6c1ae2
UraN9 <kwuvezzzkuy@gmail.com>
2025-11-11 12:01:39 +0200

    Ще пройшлися по coverage (коли є частина тесту розгалужена циклом if/elif/else)

--- lab/file_module.py
+++ lab/file_module.py
@@ -4,5 +4,14 @@ import random
 INITIAL_WORDS = ["apple", "banana", "cherry", "orange", "Python", "testing", "module", "function", "variable", "exception", "Developer"]
 def get_n_random_words(n: int) -> list[str]:
     if n > len(INITIAL_WORDS):
+        print("Неможливо згенерувати запитувану кількість слів.")
         raise ValueError("Запитуване число слів перевищує доступну кількість слів.")
+    elif n != int(n):
+        print("Кількість слів має бути цілим числом.")
+        raise ValueError("Кількість слів має бути цілим числом.")
+    elif n <= 0:
+        print("Кількість слів не може бути від'ємною.")
+        raise ValueError("Кількість слів не може бути від'ємною.")
+    else:
+        print(f"Генеруємо {n} випадкових слів для вгадування.")
     return [w.lower() for w in random.sample(INITIAL_WORDS, n)]
--- lab/tests/test_file_module.py
+++ lab/tests/test_file_module.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 import pytest
 from lab.file_module import get_n_random_words
 
@@ -12,7 +13,15 @@ def test_get_n_random_words_raises_value_error():
     """
     Перевіряємо чи функція піднімає ValueError коли ми перевищуємо доступну кількість слів
     """
-    invalid_inputs = [50]
-    with pytest.raises(ValueError):
-        for n in invalid_inputs:
+    invalid_inputs = [1.5, -2, 0, 50]
+    for n in invalid_inputs:
+        with pytest.raises(ValueError):
+            N = get_n_random_words(n)
+            assert len(N) == n, f"Очікувалось ValueError для вхідного {n}"
+            
+
+def test_get_n_random_words_expect_print_outputs():
+    with patch('builtins.print') as mock_print:
+        for n in range(1, 6):
             get_n_random_words(n)
+            mock_print.assert_any_call(f"Генеруємо {n} випадкових слів для вгадування.")
